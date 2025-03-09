982db9530f48973041f6a4090677915d21d33889
Anton Lafchiev <44170834+Toneca99@users.noreply.github.com>
2025-03-09 12:54:31 +0200

    move publisher

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,14 +1,14 @@
-// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
-
 /**
  * @see https://prettier.io/docs/configuration
  * @type {import("prettier").Config}
  */
+
 const config = {
-    trailingComma: "es5",
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
-  };
-  
-  export default config;
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+};
+
+export default config;
